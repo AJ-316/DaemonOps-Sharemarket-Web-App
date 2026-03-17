9c2e5c13a0dcf6a00140ae6bdcde8f2aab49836f
Advaitya Jadhav <aadi.champion@gmail.com>
2026-03-17 12:35:34 +0530

    Initial Commit

---
+++ backend/daemonops-sharemarket/src/main/java/wissen/daemonops/sharemarket/DaemonopsSharemarketApplication.java
@@ -0,0 +1,13 @@
+package wissen.daemonops.sharemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaemonopsSharemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaemonopsSharemarketApplication.class, args);
+	}
+
+}
